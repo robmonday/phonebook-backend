58f2a08ff6c7d15cb997f43403e76a1d8d7256e3
“robmonday” <10903531+robmonday@users.noreply.github.com>
2022-12-06 18:04:29 -0500

    updated url path where frontend is looking for data, also installed cors into backend then implemented into index.js backend

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const morgan = require('morgan')
-
 const app = express()
+
+const cors = require('cors')
+app.use(cors())
+
+const morgan = require('morgan')
 morgan.token('postObj',(req) => {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postObj'))
 
@@ -89,7 +92,7 @@ app.post('/api/persons', (req, res) => {
 	res.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
