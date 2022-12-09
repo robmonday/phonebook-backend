7ffe307da5b1a0cf475b58cfa4d556e812b36724
“robmonday” <10903531+robmonday@users.noreply.github.com>
2022-12-09 17:30:17 -0500

    exercise 3.15 completed

--- index.js
+++ index.js
@@ -13,21 +13,22 @@ app.use(cors())
 // morgan.token('postObj',(req) => {return JSON.stringify(req.body)})
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postObj'))
 
+// Hoping to comment out this object soon!
 let data = {
 	"persons": [
 		{
-			"name": "Susan Cowan",
-			"number": "865-384-1490",
+			"name": "Local Person 1",
+			"number": "123",
 			"id": 1
 		},
 		{
-			"name": "Rob Monday",
-			"number": "678-561-7626",
+			"name": "Local Person 2",
+			"number": "456",
 			"id": 2
 		},
 		{
-			"name": "Richard Cowan",
-			"number": "865-740-0011",
+			"name": "Local Person 3",
+			"number": "789",
 			"id": 3
 		}
 	]
@@ -47,10 +48,10 @@ app.get('/api/persons', (req, res) => {
 	// data returned needed to be placed as value of "persons" key inside an object
 	// because this is how I originally built my frontend
 	.then( persons => {
-		const data = {}
-		data.persons = persons
-		// console.log(data)
-		res.json(data)
+		const dataObj = {}
+		dataObj.persons = persons
+		// console.log(dataObj)
+		res.json(dataObj)
 	})
 }) 
 
@@ -73,10 +74,10 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-	const id = Number(req.params.id)
-	data.persons = data.persons.filter(p => p.id !== id)
-
-	res.status(204).end()
+	Person.findByIdAndRemove(req.params.id)
+		.then(result =>
+			res.status(204).end())
+		.catch(error => next(error))
 })
 
 const generateId = () => {
@@ -109,31 +110,6 @@ app.post('/api/persons', (req, res) => {
 		res.json(savedPerson)
 	})
 })
-
-// Old Version
-// app.post('/api/persons', (req, res) => {
-// 	// console.log("POST request body", req.body)
-// 	const body = req.body // get body from POST request
-
-// 	// validation
-// 	const nameFound = data.persons.find(p => p.name === body.name)
-// 	console.log("nameFound", nameFound)
-// 	if (nameFound) {
-// 		return res.status(400).json({error: 'Name already has a record'})
-// 	} else if(!body.name) {
-// 		return res.status(400).json({error: 'No name submitted'})
-// 	} else if (!body.number) {
-// 		return res.status(400).json({error: 'No number submitted'})
-// 	} // returned value is not used anywhere, but returning, prevents rest of code from running
-
-// 	const newPerson = {
-// 		name: body.name, 
-// 		number: body.number,
-// 		id: generateId()
-// 	}
-// 	data.persons = data.persons.concat(newPerson)
-// 	res.json(newPerson)
-// })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
