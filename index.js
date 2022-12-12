b638abb8a0863015967d472a7f4b5f6530dedfa5
“robmonday” <10903531+robmonday@users.noreply.github.com>
2022-12-12 09:07:43 -0500

    exercise 3.19 completed

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.delete('/api/persons/:id', (req, res) => {
 // 	return Math.floor(Math.random() * 10**8)
 // }
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
 	// console.log("POST request body", req.body)
 	const body = req.body // get body from POST request
 
@@ -107,9 +107,11 @@ app.post('/api/persons', (req, res) => {
 		// id: generateId()
 	})
 	
-	newPerson.save().then(savedPerson => {
-		res.json(savedPerson)
-	})
+	newPerson.save()
+		.then(savedPerson => {
+			res.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
@@ -117,6 +119,8 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
 	}
 
 	next(error)
